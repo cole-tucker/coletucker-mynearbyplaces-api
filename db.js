b385275450ab34942ccf6e32d30def618567df64
cole-tucker <coletucker@email.arizona.edu>
2021-05-04 15:21:11 -0700

    .

--- db.js
+++ db.js
@@ -18,23 +18,18 @@ let connection = {
 
 const pool = new Pool(connection);
 
-// Methods for calls
-
-// Add a place to the Database
 let addPlace = (name, address, info) => {
     return pool.query('insert into mynearbyplaces.place(name,address,info) values ($1,$2,$3)',[name, address, info])
     .then(() => console.log(`The place ${name} was added to the Data Base.`))
     .catch(e => console.log(e));
 }
 
-// Get all of the places from the Database
 let getPlaces = () => {
     let sql = `select * from mynearbyplaces.place`;
     return pool.query(sql)
     .then(result => result.rows);
 }
 
-// Add a review to the Database
 let addReview = (username, comment, placename) => {
     return pool.query('insert into mynearbyplaces.review (username, comment, placename) values ($1,$2,$3)',[username, comment, placename])
     .then(() => console.log(`The review by ${username} was added to the Data Base.`))
